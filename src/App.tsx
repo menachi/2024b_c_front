6bb5b5be9c3cc53ed4050b2269331737b900462f
Eliav Menachi <eliav.menachi@gmail.com>
2024-04-24 21:10:22 +0300

    states and props

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
 
-function App() {
-  const [count, setCount] = useState(0)
+interface PostProps {
+  title: string
+  content: string[]
+  onItemSelected: (index: number) => void
+}
 
+function PostList({ title, content, onItemSelected }: PostProps) {
+  console.log('Post')
+  const [clickedIndex, setClickedIndex] = useState(-1)
+
+  const onClick = (index: number, item: string): void => {
+    console.log('clicked on item ', index, item)
+    setClickedIndex(index)
+    onItemSelected(index)
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>{title}</h1>
+      {(content.length == 0) && <p>No Items</p>}
+      <ul className="list-group">
+        {
+          content.map(
+            (item, index) => {
+              return (
+                <li
+                  className={(clickedIndex == index) ? "list-group-item active" : "list-group-item"}
+                  key={index}
+                  onClick={() => onClick(index, item)}
+                >An item: {item} index {index}</li>
+              )
+            }
+          )
+        }
+      </ul>
     </>
+  )
+}
+
+function App() {
+  const [displayAlert, setDisplayAlert] = useState(false)
+  console.log('App')
+  const array = ['An item', 'A second item', 'A third item', 'A fourth item', 'And a fifth one']
+  const array2 = ['black', 'white', 'red', 'blue', 'green']
+  return (
+    <div>
+      {(displayAlert) &&
+        <div className="alert alert-warning alert-dismissible fade show" role="alert">
+          <strong>Holy guacamole!</strong> You should check in on some of those fields below.
+          <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close" onClick={() => setDisplayAlert(false)}></button>
+        </div>
+      }
+      <PostList title="This My Title" content={array} onItemSelected={(index) => { console.log("item selected iun first list" + index) }} />
+      <PostList title="This My Colors" content={array2} onItemSelected={(index) => { console.log("color selected " + array2[index]) }} />
+      <button type="button" className="btn btn-primary" onClick={() => { setDisplayAlert(true) }}>Display Alert</button>
+    </div>
+
   )
 }
 
